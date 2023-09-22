a45a91c7f9a7761cbef3180dbeafdc82a4498164
OrionZ12 <sulungismanara@gmail.com>
2023-09-22 09:42:01 +0700

    kuis 1

---
+++ 1.pas
@@ -0,0 +1,14 @@
+program soal1;
+var a,b,c,d: String;
+begin
+    write('Masukkan nama anda ');
+    readln(a);
+    write('Masukkan NIM anda ');
+    readln(b);
+    write('Masukkan KOM anda ');
+    readln(c);
+    write('Masukkan Alamat anda ');
+    readln(d);
+    writeln('Halo, nama saya ', a, ' NIM saya ', b, ' saya di kom ', c, ' saya tinggal di ', d, '. Salam Kenal Semua');
+
+end.
---
+++ 2.pas
@@ -0,0 +1,14 @@
+program soal2;
+var a,b,c,d: String;
+begin
+    write('Masukkan Jalan ');
+    readln(a);
+    write('Masukkan No ');
+    readln(b);
+    write('Masukkan Kota ');
+    readln(c);
+    write('Masukkan Kode Pos ');
+    readln(d);
+    writeln('Jalan ', a, ' No. ', b,' ', c, ' ', d);
+
+end.
